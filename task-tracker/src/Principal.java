dc87e137bdd1a78d4bf9fba633e396a89b76e382
Mónica Juárez <monijrzesp@hotmail.com>
2025-08-04 23:41:22 -0600

    Se agrega menú switch

--- task-tracker/src/Principal.java
+++ task-tracker/src/Principal.java
@@ -1,6 +1,33 @@
+import java.util.Scanner;
+
 public class Principal {
     public static void main(String[] args) {
+        mostrarMenu();
+    }
 
+    private static void mostrarMenu() {
+        //Se crea línea de comando para ingresar las opciones ofrecidas en el menú.
+        Scanner comando = new Scanner(System.in);
+
+
+        while (!(comando.nextLine().toLowerCase().equals("salir"))){
+            switch(comando){
+                case "agregar" -> agregarTarea();
+                case "modificar" -> modificarTarea();
+                case "eliminar" -> eliminarTarea();
+                case "comenzar" -> comenzarTarea();
+                case "finalizar" -> finalizarTarea();
+                case "listar" ->listarTareas();
+                case "tareas en progreso" -> listarTareasEnProgreso();
+                case "tareas terminadas" -> listarTareasTerminadas();
+                case "menu" -> mostrarMenu();
+                default -> System.out.println("Comando no encontrado. Escribe 'menu' para ver las opciones.");
+            }
+        }
+    salir();
+    }
+
+    private static void salir() {
     }
 
     private static void listarTareasTerminadas() {
