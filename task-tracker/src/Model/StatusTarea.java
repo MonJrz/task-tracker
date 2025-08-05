02bce971236c22f6732ff8f8d47688a1afb9eb0d
Mónica Juárez <monijrzesp@hotmail.com>
2025-08-04 19:46:30 -0600

    Se agrega Model

---
+++ task-tracker/src/Model/StatusTarea.java
@@ -0,0 +1,9 @@
+package Model;
+
+public enum StatusTarea {
+    PENDIENTE,
+    EN_PROGRESO,
+    FINALIZADA;
+    
+
+}
---
+++ task-tracker/src/Model/Tarea.java
@@ -0,0 +1,10 @@
+package Model;
+import java.time.LocalDateTime;
+
+public class Tarea {
+    private long id;
+    private String descripcion;
+    private StatusTarea status;
+    private LocalDateTime fechaCreacion;
+    private LocalDateTime fechaTermino;
+}
